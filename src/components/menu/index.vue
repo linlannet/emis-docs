30606c1b9615ddc3565f1e9a4d72156275913a90
lupeiran <lupeiran@wondersgroup.com>
2025-08-19 16:23:22 +0800

    路径修改为绝对路径

--- src/components/menu/index.vue
+++ src/components/menu/index.vue
@@ -16,59 +16,59 @@ const handleNodeClick = (data: Tree) => {
 const data: Tree[] = [
   {
     label: "What's new",
-    url: '../docs/README.md',
+    url: '/docs/README.md',
   },
   {
     label: "文档使用指南",
-    url: '../docs/allguide.md',
+    url: '/docs/allguide.md',
   },
   {
     label: '系统全景图',
-    url: '../docs/overview-platform.md',
+    url: '/docs/overview-platform.md',
     children: [
       {
         label: '专业术语',
-        url: '../docs/overview/term.md'
+        url: '/docs/overview/term.md'
       },
       {
         label: '系统简介',
-        url: '../docs/overview/introduction.md'
+        url: '/docs/overview/introduction.md'
       },
       {
         label: '系统架构',
-        url: '../docs/overview/architecture.md'
+        url: '/docs/overview/architecture.md'
       },
       {
         label: '安全特性',
-        url: '../docs/overview/security.md'
+        url: '/docs/overview/security.md'
       },
       {
         label: '信创支持',
-        url: '../docs/overview/innovation.md'
+        url: '/docs/overview/innovation.md'
       },
       {
         label: '系统荣誉',
-        url: '../docs/overview/honors.md'
+        url: '/docs/overview/honors.md'
       }
     ],
   },
   {
     label: '产品简介',
-    url: '../docs/guide/README.md',
+    url: '/docs/guide/README.md',
     children: [
       {
         label: '管理控制台',
         children: [{
           label: '登录',
-          url: '../docs/guide/console/login.md'
+          url: '/docs/guide/console/login.md'
         }, {
           label: '总览',
-          url: '../docs/guide/console/workspace.md'
+          url: '/docs/guide/console/workspace.md'
         }]
       },
       {
         label: '门户工作台',
-        url: '../docs/overview/introduction.md'
+        url: '/docs/overview/introduction.md'
       }
     ]
   }
